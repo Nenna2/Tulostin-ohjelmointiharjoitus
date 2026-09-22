47f4c1dc4f8c384a03e733e2c9346c50b1049774
nenna <nennaemilia@icloud.com>
2026-09-22 14:16:40 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+String tekija = "Nenna";
+
+        System.out.println("Hei olen Tulostin-ohjelma");
+        System.out.print("Ohjelman tekijä: ");
+        System.out.println(tekija);
+    }
+}
