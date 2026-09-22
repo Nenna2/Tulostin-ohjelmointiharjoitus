cdffc6ee20cfebfa62e6500d39a6457601bc6216
nenna <nennaemilia@icloud.com>
2026-09-22 14:54:55 +0300

    Järjestetty muuttujat ja lisätty laskutoimitukset

--- src/App.java
+++ src/App.java
@@ -1,24 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
+// Muuttujat
 String tekija = "Nenna";
-
-        System.out.println("Hei olen Tulostin-ohjelma");
-        System.out.print("Ohjelman tekijä: ");
-        System.out.println(tekija);
-        
 double luku1 = 5;
 double luku2 = 2;
 
-System.out.println("Luku1-muuttujan arvo on " + luku1);
-System.out.println("Luku2-muuttujan arvo on " + luku2);
-
 double tulo = luku1 * luku2;
-
-System.out.println(luku1 + " * " + luku2 + " = " + tulo);
 double erotus = luku1 - luku2;
 double summa = luku1 + luku2;
 double jako = luku1 / luku2;
 
+
+// Tulostukset
+System.out.println("Hei olen Tulostin-ohjelma");
+
+System.out.print("Ohjelman tekijä: ");
+System.out.println(tekija);
+
+System.out.println("Luku1-muuttujan arvo on " + luku1);
+System.out.println("Luku2-muuttujan arvo on " + luku2);
+System.out.println(luku1 + " * " + luku2 + " = " + tulo);
 System.out.println(luku1 + " - " + luku2 + " = " + erotus);
 System.out.println(luku1 + " + " + luku2 + " = " + summa);
 System.out.println(luku1 + " / " + luku2 + " = " + jako);      
