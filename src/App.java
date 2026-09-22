7fedf5dba59ee4bee3af4f1894ff2f3700c45a3a
nenna <nennaemilia@icloud.com>
2026-09-22 14:37:36 +0300

    Lisää vähennys-, summa- ja jakolaskut

--- src/App.java
+++ src/App.java
@@ -14,6 +14,13 @@ System.out.println("Luku2-muuttujan arvo on " + luku2);
 
 double tulo = luku1 * luku2;
 
-System.out.println(luku1 + " * " + luku2 + " = " + tulo);      
+System.out.println(luku1 + " * " + luku2 + " = " + tulo);
+double erotus = luku1 - luku2;
+double summa = luku1 + luku2;
+double jako = luku1 / luku2;
+
+System.out.println(luku1 + " - " + luku2 + " = " + erotus);
+System.out.println(luku1 + " + " + luku2 + " = " + summa);
+System.out.println(luku1 + " / " + luku2 + " = " + jako);      
     }
 }
